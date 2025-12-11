0aa1cdef2551590b069dc2d0f6010e44d031f27b
nicojaee <nicholasjason11@gmail.com>
2025-12-11 21:13:39 +0700

    fix: auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
-
+        Auth::login($user);
         return redirect('/');
     }
 
