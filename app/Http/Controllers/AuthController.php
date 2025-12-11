05a61620006872de56e201705e3770a114eb139d
nicojaee <nicholasjason11@gmail.com>
2025-12-11 20:52:03 +0700

    fix: auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
-        if(Auth::attemp(only('email', 'password'))){
+        if(Auth::attemp($request->only('email', 'password'))){
             return redirect()->intended(route('home'));
         }
         return back()->withErrors([
