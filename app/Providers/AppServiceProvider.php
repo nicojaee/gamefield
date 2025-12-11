b6405a868b1d88061ab2fc7064d928dd2d62ff78
nicojaee <nicholasjason11@gmail.com>
2025-12-11 20:53:37 +0700

    fix: kurang titik koma

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades
+use Illuminate\Support\Facades;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
